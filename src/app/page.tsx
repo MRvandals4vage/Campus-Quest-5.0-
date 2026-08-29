9e9a04af857a2fd66bb515fe1eeca4a5bd6c24f8
MRvandals4vage <iu9736@srmist.edu.in>
2026-08-29 23:04:31 +0530

    all balance changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,21 @@ import Chatbot from "@/components/Chatbot";
 export default function Home() {
   return (
     <main className="bg-black text-white">
-      <Hero />
-      <About />
-      <Timeline />
-      <SponsorPage />
-      <PastEvents />
+      <div>
+        <Hero />
+      </div>
+      <div className="section-wrap">
+        <About />
+      </div>
+      <div className="section-wrap-mid">
+        <Timeline />
+      </div>
+      <div className="section-wrap-mid">
+        <SponsorPage />
+      </div>
+      <div className="section-wrap-mid">
+        <PastEvents />
+      </div>
       <Footer />
       <Chatbot />
     </main>
